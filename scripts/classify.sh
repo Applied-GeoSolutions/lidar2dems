f0b0dfcc4cc87009f7b2a9ee5c40f5c4fc89fac9
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-03-20 09:33:54 -0400

    updated classify

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -2,14 +2,17 @@
 
 # Arguments:  directory, slope, cellsize
 
-FILENAMES=$1/*.las
+FILENAMES=$1/*0.las
 
 date
-echo "File count = " `ls -1 *.las | wc -l`
+echo "File count = " `ls -1 $FILENAMES | wc -l`
 
 for FNAME in $FILENAMES; do
     # run pdal ground
-    cmd="time pdal ground -i $FNAME -o ${FNAME%.las*}_pg_s$2_c$3.las --slope $2 --cellSize $3 --classify"; # --writers.las.format=0";
-    echo $cmd
-    eval $cmd
+    FOUT=${FNAME%.las*}_pg_s$2_c$3.las
+    if [ ! -f $FOUT ]; then
+        cmd="time pdal ground -i $FNAME -o $FOUT --slope $2 --cellSize $3 --classify"; # --writers.las.format=0";
+        echo $cmd
+        eval $cmd
+    fi
 done
