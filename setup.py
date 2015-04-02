2d5a35c545d0073f7f35ef8d88464c6cfb246af0
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-04-02 09:06:30 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup
 import imp
+import glob
+import traceback
 
 __version__ = imp.load_source('lidar2dems.version', 'lidar2dems/version.py').__version__
+
+scripts = []
+for f in glob.glob('l2d/scripts/*.py'):
+    try:
+        name = os.path.splitext(os.path.basename(f))[0]
+        if name not in ['__init__']:
+            scripts.append('l2d_%s = l2d.scripts.%s:main' % (name, name.lower()))
+    except:
+        print traceback.format_exc()
 
 setup(
     name='lidar2dems',
@@ -13,5 +25,5 @@ setup(
     author_email='matt.a.hanson@gmail.com',
     packages=['l2d'],
     requirements=['gippy'],
-#    entry_points={'console_scripts': console_scripts}
+    entry_points={'console_scripts': scripts}
 )
