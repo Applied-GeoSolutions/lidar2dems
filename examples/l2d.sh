e24acdb621576d945f893e50d420a286bb078f5d
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-04-02 12:11:24 -0400

    fixed typo

--- examples/l2d.sh
+++ examples/l2d.sh
@@ -15,4 +15,4 @@ l2d_classify LAS/*0.las -s 1 -c 3 --outdir Classified_LAS
 l2d_createdems Classified_LAS/*c3.las --dsm 0.56419 --dtm 0.56419 1.0 1.4142 2.0 2.5 3.0 --bounds $2 $3 $4 $5--epsg $EPSG --outdir DEMs
 
 # create vrt of photos
-gdalbuildvt photos_lowres.vrt lowres_photos/*.tif
+gdalbuildvrt photos_lowres.vrt lowres_photos/*.tif
