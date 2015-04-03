a3c466607ff059e1f9b2ebea7888d4471a32803a
Franklin Sullivan <fsulliva@cerrado.sr.unh.edu>
2015-04-03 12:36:25 -0400

    fixed demdir and outdir in createdems.py which threw error at create_vrts() because of incorrect directory

--- l2d/scripts/createdems.py
+++ l2d/scripts/createdems.py
@@ -4,7 +4,7 @@
 This script processes LAS file(s) and creates a DSM and series of DTM's using different radii when gridding
 """
 
-import os
+import os, sys
 import argparse
 import glob
 from datetime import datetime
@@ -15,7 +15,9 @@ def run(filenames, dsm, dtm, epsg, bounds, outdir):
     """ Process files to create DEMS """
     if not os.path.exists(outdir):
         os.makedirs(outdir)
-    demdir = os.path.join(outdir, 'DEMs')
+    demdir = os.path.join(outdir, 'Final/') #this directory needs to be created
+    if not os.path.exists(demdir):
+        os.makedirs(demdir) #now we can output to it.
     numfiles = len(filenames)
     print 'Processing %s LAS files into DEMs' % numfiles
     for i, f in enumerate(filenames):
@@ -24,19 +26,22 @@ def run(filenames, dsm, dtm, epsg, bounds, outdir):
         create_dems(os.path.abspath(f), dsm, dtm, epsg, outdir=outdir)
 
         print 'Processed (%s of %s) %s in %s' % (i + 1, numfiles, f, datetime.now() - start)
+        
+        sys.stdout.flush()
 
     print 'Creating VRT files'
-    create_vrts(demdir, bounds=bounds)
+    #is this supposed to be given a directory or a list of files? directory works but the outdir should be the demdir...
+    create_vrts(outdir, bounds=bounds)
 
     print 'Gap-filling and creating final DEMs'
     # create gap-filled versions of DSM and DTM
-    dsm_pattern = glob.glob(os.path.join(demdir, 'DSM*max.vrt'))
-    dtm_pattern = glob.glob(os.path.join(demdir, 'DTM*idw.vrt'))
-    dsm = gap_fill(dsm_pattern, os.path.join(outdir, 'DSM.tif'), interpolation='nearest')
-    dtm = gap_fill(dtm_pattern, os.path.join(outdir, 'DTM.tif'), interpolation='nearest')
+    dsm_pattern = glob.glob(os.path.join(outdir, 'DSM*max.vrt'))
+    dtm_pattern = glob.glob(os.path.join(outdir, 'DTM*idw.vrt'))
+    dsm = gap_fill(dsm_pattern, os.path.join(demdir, 'DSM.tif'), interpolation='nearest')
+    dtm = gap_fill(dtm_pattern, os.path.join(demdir, 'DTM.tif'), interpolation='nearest')
 
     # create CHM
-    chm = create_chm(dtm, dsm, os.path.join(outdir, 'CHM.tif'))
+    chm = create_chm(dtm, dsm, os.path.join(demdir, 'CHM.tif'))
 
     # create hillshades
     for f in [dsm, dtm, chm]:
