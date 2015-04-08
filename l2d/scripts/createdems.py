cd51c9ba88b7110888c92c9133a8c8d9b219ee41
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-04-08 13:46:05 -0400

    added outlier argument

--- l2d/scripts/createdems.py
+++ l2d/scripts/createdems.py
@@ -19,6 +19,7 @@ def main():
     parser.add_argument('--dsm', help='Create DSM (run for each provided radius)', nargs='*', default=[])
     parser.add_argument('--dtm', help='Create DTM (run for each provided radius)', nargs='*', default=[])
     parser.add_argument('--epsg', help='EPSG code to assign to DEM outputs')
+    parser.add_argument('--outliers', help='Filter outliers with this StdDev threshold in the DSM', default=3.0)
     parser.add_argument('--bounds', help='Bounds (xmin ymin xmax ymax) of output files (VRTs)', default=None, nargs=4)
     parser.add_argument('--outdir', help='Output directory', default='./')
     args = parser.parse_args()
@@ -26,7 +27,8 @@ def main():
     start = datetime.now()
     numfiles = len(args.filenames)
     print 'Processing %s LAS files into DEMs' % numfiles
-    create_dems(args.filenames, args.dsm, args.dtm, epsg=args.epsg, bounds=args.bounds, outdir=args.outdir)
+    create_dems(args.filenames, args.dsm, args.dtm, epsg=args.epsg,
+                bounds=args.bounds, outliers=args.outliers, outdir=args.outdir)
     print 'Processed in %s' % (datetime.now() - start)
 
 
