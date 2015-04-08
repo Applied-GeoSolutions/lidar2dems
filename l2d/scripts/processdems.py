1d4ad880be288fd309f4c4662dc6356de15678a7
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-04-08 13:46:38 -0400

    added verbosity and properly check for tif not vrt files

--- l2d/scripts/processdems.py
+++ l2d/scripts/processdems.py
@@ -6,8 +6,9 @@ This processes a series of DSM and DTM files to gap fill and create hillshaded v
 
 import os
 import argparse
-import datetime
+import datetime as dt
 import glob
+import gippy
 from l2d import gap_fill, create_chm
 
 
@@ -19,17 +20,19 @@ def main():
     parser.add_argument('--indir', help='Input directory containing DEMs', default='./')
     parser.add_argument('--outdir', help='Output directory', default='./')
     parser.add_argument('--interp', help='Interpolation method (nearest, linear, cubic)', default='nearest')
+    parser.add_argument('-v', '--verbose', help='Verbosity level', type=int, default=1)
     args = parser.parse_args()
 
-    start = datetime.now()
+    start = dt.datetime.now()
+    gippy.Options.SetVerbose(args.verbose)
     print 'Gap-filling and creating final DEMs'
 
     # create gap-filled versions of DSMs
-    dsm_files = glob.glob(os.path.join(args.indir, 'DSM*max.vrt'))
+    dsm_files = glob.glob(os.path.join(args.indir, 'DSM*max.tif'))
     dsm = gap_fill(dsm_files, os.path.join(args.outdir, 'DSM.tif'), interpolation=args.interp)
 
     # create gap-filled versions of DTMs
-    dtm_files = glob.glob(os.path.join(args.indir, 'DTM*idw.vrt'))
+    dtm_files = glob.glob(os.path.join(args.indir, 'DTM*idw.tif'))
     dtm = gap_fill(dtm_files, os.path.join(args.outdir, 'DTM.tif'), interpolation=args.interp)
 
     # create CHM
@@ -40,7 +43,7 @@ def main():
         cmd = 'gdaldem hillshade %s %s' % (f, os.path.splitext(f)[0] + '_hillshade.tif')
         os.system(cmd)
 
-    print 'Processed in %s' % (datetime.now() - start)
+    print 'Processed in %s' % (dt.datetime.now() - start)
 
 
 if __name__ == "__main__":
