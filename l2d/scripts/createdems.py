7b23e493f3b8dc4504125b865aeb9e7081698867
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-04-29 14:45:14 -0400

    refactored scripts

--- l2d/scripts/createdems.py
+++ l2d/scripts/createdems.py
@@ -7,9 +7,8 @@ LAS files will be merged together as a single point cloud for processing
 
 import argparse
 from datetime import datetime
-from l2d import create_dems, check_boundaries
+from l2d import create_dems2, check_boundaries
 import gippy
-from math import floor, ceil
 
 
 def main():
@@ -20,30 +19,25 @@ def main():
     parser.add_argument('filenames', help='Classified LAS file(s) to process', nargs='+')
     parser.add_argument('--dsm', help='Create DSM (run for each provided radius)', nargs='*', default=[])
     parser.add_argument('--dtm', help='Create DTM (run for each provided radius)', nargs='*', default=[])
-    parser.add_argument('--epsg', help='EPSG code to assign to DEM outputs')
     parser.add_argument('--outliers', help='Filter outliers with this StdDev threshold in the DSM', default=3.0)
-    # h = 'Bounds (xmin ymin xmax ymax) of output files (VRTs)'
-    # parser.add_argument('--bounds', help=h, default=None, nargs=4, type=float)
-    parser.add_argument('-s', '--shapefile', help='Shapefile with bounds', default=None)
+    parser.add_argument('--maxangle', help='Filter out points whose absolute value of scan angle is larger', default=None)
+    parser.add_argument('-s', '--shapefile', help='Shapefile with correct projection and desired bounds', required=True)
     parser.add_argument('--outdir', help='Output directory', default='./')
     args = parser.parse_args()
 
     start = datetime.now()
 
-    # get bounds from shapefile
-    if args.shapefile is not None:
-        extent = gippy.GeoVector(args.shapefile).Extent()
-        bounds = [floor(extent.x0()), floor(extent.y0()), ceil(extent.x1()), ceil(extent.y1())]
+    # open vector file
+    vector = gippy.GeoVector(args.shapefile)
 
-    if bounds is not None:
-        filenames = check_boundaries(args.filenames, bounds)
-    else:
-        filenames = args.filenames
+    # filter out list with only intersecting files
+    filenames = check_boundaries(args.filenames, vector)
 
     numfiles = len(filenames)
     print 'Processing %s LAS files into DEMs' % numfiles
-    create_dems(filenames, args.dsm, args.dtm, epsg=args.epsg,
-                bounds=bounds, outliers=args.outliers, outdir=args.outdir)
+    create_dems2(filenames, args.dsm, args.dtm, vector=vector,
+                 outliers=args.outliers, maxangle=args.maxangle, outdir=args.outdir)
+
     print 'Processed in %s' % (datetime.now() - start)
 
 
--- l2d/scripts/filldems.py
+++ l2d/scripts/filldems.py
@@ -4,10 +4,9 @@
 Combined a series of DEM and gap fill.  Optionally regrid using provided shapefile
 """
 
-import os
 import argparse
 import datetime as dt
-from l2d import warp_and_clip_image, gap_fill
+from l2d import gap_fill, create_hillshade
 
 
 def main():
@@ -16,32 +15,22 @@ def main():
     desc = 'Create final gap-filled DEMs using multiple radius versions if provided'
     parser = argparse.ArgumentParser(description=desc, formatter_class=dhf)
     parser.add_argument('filenames', help='DEM files to combine and fill', nargs='+')
-    h = 'If provided use bounds from shapefile to warp input files first'
+    h = 'Clip final image to this shapefile'
     parser.add_argument('-s', '--shapefile', help=h, default=None)
     parser.add_argument('--fout', help='Output filename', default='DEM.tif')
     parser.add_argument('--hillshade', help='Generate hillshade', default=False, action='store_true')
     parser.add_argument('--interp', help='Interpolation method (nearest, linear, cubic)', default='nearest')
-    #parser.add_argument('-v', '--verbose', help='Verbosity level', type=int, default=1)
     args = parser.parse_args()
 
     start = dt.datetime.now()
-    #gippy.Options.SetVerbose(args.verbose)
     print 'Gap-filling and creating final DEMs from %s files' % len(args.filenames)
 
-    if args.shapefile is not None:
-        filenames = []
-        for f in args.filenames:
-            filenames.append(warp_and_clip_image(f, args.shapefile))
-    else:
-        filenames = args.filenames
-
-    fout = gap_fill(filenames, args.fout, shapefile=args.shapefile, interpolation=args.interp)
+    fout = gap_fill(args.filenames, args.fout, shapefile=args.shapefile, interpolation=args.interp)
 
     if args.hillshade:
-        cmd = 'gdaldem hillshade %s %s' % (fout, os.path.splitext(fout)[0] + '_hillshade.tif')
-        os.system(cmd)
+        create_hillshade(fout)
 
-    print 'Processed in %s' % (dt.datetime.now() - start)
+    print 'Completed in %s' % (dt.datetime.now() - start)
 
 
 if __name__ == "__main__":
