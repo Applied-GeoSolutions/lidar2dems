d6bbefcae3a9134e8af236b89127a868556ffc6d
Matthew Hanson <mhanson@appliedgeosolutions.com>
2015-05-11 15:36:21 -0400

    gapfilling and comining finals

--- l2d/scripts/dems.py
+++ l2d/scripts/dems.py
@@ -6,8 +6,11 @@ Creates density image of all files
 
 import os
 from datetime import datetime
-from l2d import l2dParser, create_dem, create_dem_piecewise, gap_fill
+import glob
+from l2d import find_lasfiles, create_dems, gap_fill
+from l2d.parsers import l2dParser
 from gippy import GeoVector
+import traceback
 
 
 def main():
@@ -19,61 +22,84 @@ def main():
 
     start0 = datetime.now()
 
-    # open vectors
+    # open site vector
     if args.site is not None:
         try:
-            args.site = GeoVector(args.site)[0]
+            site = GeoVector(args.site)
         except:
             print 'Error opening %s' % args.site
             exit(2)
-    if args.features is not None:
-        try:
-            args.features = GeoVector(args.features)
-        except:
-            print 'Error opening %s' % args.features
-            exit(2)
+    else:
+        site = [None]
 
     # make sure outdir exists
+    args.outdir = os.path.abspath(args.outdir)
     if not os.path.exists(args.outdir):
         os.makedirs(args.outdir)
 
+    args.lasdir = os.path.abspath(args.lasdir)
+
     # loop through features
-    for i, feature in enumerate(args.features): 
-        # classify ? 
+    fouts_site = []
+    products = dem_products(args.demtype)
+    bnames = {p: '%s%s.%s' % (args.demtype, args.suffix, p) for p in products}
+    for feature in site: 
         # get classified las filename
-        las =   
+        if args.demtype == 'density':
+            # find all las files within feature
+            lasfiles = find_lasfiles(args.lasdir, site=feature)
+        else:
+            # assume single classified las file exists for this feature
+            lasfiles = [glob.glob(os.path.join(args.lasdir, feature.Basename() + '*.las'))[0]]
+        print lasfiles
 
-    # run piecewise if given processing features
-    if args.features is None:
-        del args.features
-        func = lambda **kwargs: create_dem(**kwargs)
-    else:
-        func = lambda **kwargs: create_dem_piecewise(**kwargs)
+        # pull out the arguments to pass to create_dems
+        keys = ['radius', 'decimation', 'maxsd', 'maxz', 'maxangle', 'returnnum', 
+                'outdir', 'suffix', 'verbose']
+        vargs = vars(args)
+        kwargs = {k:vargs[k] for k in vargs if k in keys}
 
-    # create dems for each given radius
-    radii = args.radius 
-    del args.radius
-    gapfill = args.gapfill
-    del args.gapfill
-    fouts = []
-    try:
-        for rad in radii:
-            fouts.append(func(radius=rad, **vars(args)))
-    except Exception, e:
-        print 'Error creating %s: %s' % (args.demtype, e)
-        exit(2)
+        # create the dems
+        try:
+            fouts = create_dems(lasfiles, args.demtype, site=feature, **kwargs)
+            print fouts
+        except Exception, e:
+            if args.verbose:
+                print traceback.format_exc()
+            print 'Error creating %s: %s' % (args.demtype, e)
+            exit(2)
 
+        # gap-fill each product (except density)
+        if args.gapfill and args.demtype != 'density':
+            _fouts = {}
+            for product in fouts[0].keys():
+                print product
+                if product == 'den':
+                    continue
+                # input filenames
+                fnames = [f[product] for f in fouts]
+                # output filename
+                bname = if feature is None else feature.Basename() + '_'
+                fout = os.path.join(args.outdir, bname + bnames[product] + '.tif')
+                gap_fill(fnames, fout, site=feature)
+                _fouts[product] = [fout]
+            fouts = _fouts
 
-    # gap-fill each type of output file
-    if gapfill:
-        _fouts = []
-        for product in fouts[0].keys():
-            if product != 'den':
-                filenames = [f[product] for f in fouts]
-                fout = os.path.join(args.outdir, '%s%s.%s.tif' % (args.demtype, args.suffix, product))
-                gap_fill(filenames, fout, site=args.site)
-                _fouts.append(fout)
-        fouts = _fouts
+        print fouts
+
+        fouts_site.append(fouts)
+
+    print fouts_site
+
+    # combine all polygons into single file and align to site
+    fouts = {}
+    for product in fouts_site[0].keys():
+        fnames = [f[product] for f in fouts_site]
+        fout = os.path.join(args.outdir, '%s%s.%s.tif' % args.demtype, args.suffix, product)
+        combine(fnames, fout, site=site)
+        fouts[product] = fout
+
+    print fouts
 
     print 'Completed %s (%s) in %s' % (args.demtype, args.outdir, datetime.now() - start0)
 
