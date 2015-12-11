4baa04fb29d0a1f6d3bca0607fa1cb61330de387
F-Sullivan <fsulliva@gmail.com>
2015-12-11 15:13:08 -0500

    changed product fouts

--- l2d/scripts/process_voxels.py
+++ l2d/scripts/process_voxels.py
@@ -70,7 +70,7 @@ def main():
     # the final filenames
     product = args.metric
     #fouts = os.path.join(args.outdir, '%s.voxel_metric.vrt' % (product))
-    fouts = {p: os.path.join(args.outdir, '%s.voxel_metric.vrt' % (p)) for p in product}
+    fouts = {p: os.path.join(args.outdir, '%s.voxel_metric.vrt' % (p)) for p in [product]}
 
     # run if any products are missing
     exists = all([os.path.exists(f) for f in fouts.values()])
